aad575ec4a3cc90ff85eb72fe0951860510dafd5
ryjm <jake@urbit.land>
2024-09-18 16:18:47 -0400

    fix ToBytes for str

--- crown/src/utils/bytes.rs
+++ crown/src/utils/bytes.rs
@@ -103,13 +103,9 @@ impl ToBytes for Vec<u8> {
         Ok(self.clone())
     }
 }
-
 impl ToBytes for &str {
     fn to_bytes(&self) -> Result<Vec<u8>> {
-        if let Ok(data) = self.as_bytes() {
-            Ok(data.to_vec())
-        } else {
-            Err(CrownError::Unknown("ToBytes".to_string()))
-        }
+        let bytes = self.bytes();
+        Ok(bytes.collect::<Vec<u8>>())
     }
 }
