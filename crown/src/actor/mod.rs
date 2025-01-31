e1bfd001fa2c4368612866a53973add293113100
Edward Amsden <edward@blackriversoft.com>
2025-01-31 15:17:06 -0600

    More drafting of an actor model

--- crown/src/actor/mod.rs
+++ crown/src/actor/mod.rs
@@ -1,79 +1,115 @@
 use std::future::Future;
+use std::pin::Pin;
 
 use tokio::sync::mpsc;
 use tokio::task::JoinHandle;
 
 // enumerate errors that actors may terminate with
-enum ActorError {}
+pub enum ActorError {
+    JoinError(tokio::task::JoinError),
+}
+
+impl From<tokio::task::JoinError> for ActorError {
+    fn from(err: tokio::task::JoinError) -> Self {
+        ActorError::JoinError(err)
+    }
+}
 
 // result of an actor (when joining)
 type ActorResult = Result<(), ActorError>;
 
-// result of polling an actor's status
-enum ActorStatus {
-    ActorError(ActorError),
-    ActorRunning,
-    ActorDone,
+// errors when sending a message to an actor
+pub enum ActorSendError<M> {
+    MpscSendError(mpsc::error::SendError<M>),
 }
 
-// errors when sending a message to an actor
-enum ActorSendError<M> {
-    _Oops(M),
+impl<M> From<mpsc::error::SendError<M>> for ActorSendError<M> {
+    fn from(err: mpsc::error::SendError<M>) -> Self {
+        ActorSendError::MpscSendError(err)
+    }
 }
 
 // result when sending a message to an actor
-type ActorSendResult<M> = Result<(), ActorSendError<M>>;
+pub type ActorSendResult<M> = Result<(), ActorSendError<M>>;
 
-enum ActorReceiveError<M> {
-    _Oops(M),
+pub enum ActorReceiveError {
+    MailboxClosed,
 }
 
-type ActorReceiveResult<M> = Result<M, ActorReceiveError<M>>;
+pub type ActorReceiveResult<M> = Result<M, ActorReceiveError>;
 
 /// returned when an actor is spawned
-struct ActorHandle<M> {
-    handle: JoinHandle<ActorResult>,
-    mailbox: mpsc::Sender<M>,
+pub struct ActorHandle<M> {
+    handle: JoinHandle<()>,
+    dropbox: Dropbox<M>,
 }
 
 impl<M> ActorHandle<M>
 where
     M: Sync + Send,
 {
-    fn status(&self) -> ActorStatus {
-        todo!()
+    pub fn is_finished(&self) -> bool {
+        self.handle.is_finished()
     }
-    fn send(&self, message: M) -> ActorSendResult<M> {
-        todo!()
+
+    pub async fn join(self) -> ActorResult {
+        Ok(self.handle.await?)
     }
-    fn dropbox(&self) -> Dropbox<M> {
-        todo!()
+
+    pub async fn send(&mut self, message: M) -> ActorSendResult<M> {
+        self.dropbox.send(message).await
+    }
+    pub fn dropbox(&self) -> Dropbox<M> {
+        self.dropbox.clone()
     }
 }
 
-struct Mailbox<M> {
+pub struct Mailbox<M> {
     mailbox: mpsc::Receiver<M>,
-}
-
-struct Dropbox<M> {
-    dropbox: mpsc::Sender<M>,
 }
 
 impl<M> Mailbox<M>
 where
     M: Send + Sync,
 {
-    async fn receive(&mut self) -> Result<M, ActorReceiveError<M>> {
-        todo!()
+    pub async fn receive(&mut self) -> Result<M, ActorReceiveError> {
+        self.mailbox
+            .recv()
+            .await
+            .map_or_else(|| Err(ActorReceiveError::MailboxClosed), |x| Ok(x))
     }
-    fn has_messages(&self) -> bool {
-        todo!()
+    pub fn has_messages(&self) -> bool {
+        !self.mailbox.is_empty()
     }
 }
 
-fn spawn<F, M>(task: F) -> ActorHandle<M>
-where
-    F: FnOnce(Mailbox<M>) -> Box<dyn Future<Output = ()>>,
-{
-    todo!()
+pub struct Dropbox<M> {
+    dropbox: mpsc::Sender<M>,
 }
+
+impl<M: Send> Dropbox<M> {
+    async fn send(&mut self, message: M) -> ActorSendResult<M> {
+        Ok(self.dropbox.send(message).await?)
+    }
+}
+
+impl<M> Clone for Dropbox<M> {
+    fn clone(&self) -> Self {
+        Dropbox {
+            dropbox: self.dropbox.clone(),
+        }
+    }
+}
+
+pub fn spawn<F, M>(task: F, size: usize) -> ActorHandle<M>
+where
+    F: FnOnce(Mailbox<M>) -> Pin<Box<dyn ActorFuture>>,
+{
+    let (outbox, inbox) = mpsc::channel(size);
+    let mailbox = Mailbox { mailbox: inbox };
+    let handle = tokio::spawn(task(mailbox));
+    let dropbox = Dropbox { dropbox: outbox };
+    ActorHandle { handle, dropbox }
+}
+
+pub trait ActorFuture: Future<Output = ()> + Send {}
